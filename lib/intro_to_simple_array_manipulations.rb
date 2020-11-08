f53024282e9d95e0be4e8d772d92bd584b8b30f2
Oumaima Azzat <oumaimazzat@gmail.com>
2020-11-08 02:40:11 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,9 +15,9 @@ def using_flatten(array)
 end 
 
 def using_delete(array, string)
-  array.delete 
+  array.delete(string)
 end 
 
 def using_delete_at(array, integer)
-  array.delete_at 
+  array.delete_at(integer)
 end 
