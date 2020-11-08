c67c07ab99dcb483df792a3a22d4ff061cb86951
Oumaima Azzat <oumaimazzat@gmail.com>
2020-11-08 02:33:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,13 @@
 def using_concat(array, love)
   array.concat(love)
 end
+
+def usng_insert(array, love)
+  array.insert(love)
+end 
+
+def using_uniq(array)
+  array.uniq 
+end 
+
+def using_flatten
